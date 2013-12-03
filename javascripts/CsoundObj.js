d54b3c207263b7d18e644e17933793de8c0a2ca1
Edward Costello <edwardcostello@gmail.com>
2013-12-03 20:20:05 +0100

    Fixed license

--- javascripts/CsoundObj.js
+++ javascripts/CsoundObj.js
@@ -5,7 +5,7 @@
  Copyright (C) 2013 Edward Costello 
  This file is part of Csound Emscripten 
  
- The Csound for iOS Library is free software; you can redistribute it
+ The Csound Emscripten is free software; you can redistribute it
  and/or modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
